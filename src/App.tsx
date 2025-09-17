40d8fd81c17ea09f661ed68c458dade9ebd8aaa1
Heesunee <91944542+heesunee@users.noreply.github.com>
2025-09-17 19:50:42 +0900

    init: project setting (#2)
    * init: tailwind setting (#2)
    
    * init: route setting (#2)
    
    * init: biome.json add (#2)
    
    * init: path alias add (#2)
    
    * init: pr template add (#2)
    
    * init: typography add (#2)
    
    * init: font add (#2)
    
    ---------
    
    Co-authored-by: heesunee <heesun729@uos.ac.kr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
+import { RouterProvider } from 'react-router-dom';
+import { router } from '@routes/router';
 const App = () => {
   return (
-    <>hi</>
+      <RouterProvider router={router}/>
   )
 }
 
---
+++ src/pages/home.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <div className='title_01'>
+      Home
+    </div>
+  )
+}
+
+export default Home
