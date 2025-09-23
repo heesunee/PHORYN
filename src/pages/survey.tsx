aa135097b452a3f07970ca7d87c27545ee7e9aed
heesunee <heesun729@uos.ac.kr>
2025-09-23 16:23:08 +0900

    feat: routes 분리 및 survey page publishing (#4)

---
+++ src/pages/survey.tsx
@@ -0,0 +1,5 @@
+const Survey = () => {
+  return <main>설문조사페이지</main>;
+};
+
+export default Survey;
--- src/shared/routes/router.tsx
+++ src/shared/routes/router.tsx
@@ -2,9 +2,12 @@ import { createBrowserRouter } from 'react-router-dom';
 import Home from '@pages/home';
 import Intro from '@pages/intro';
 import NameInput from '@pages/name-input';
+import { ROUTES } from '@routes/routes-config';
+import Survey from '@pages/survey';
 
 export const router = createBrowserRouter([
-  { path: '/', element: <Home /> },
-  { path: '/intro', element: <Intro /> },
-  { path: '/name-input', element: <NameInput /> },
+  { path: ROUTES.HOME, element: <Home /> },
+  { path: ROUTES.INTRO, element: <Intro /> },
+  { path: ROUTES.NAME_INPUT, element: <NameInput /> },
+  { path: ROUTES.SURVEY, element: <Survey/>}
 ]);
